e4d995ea7c48cd1526ec06fdbf366b123743253d
wanghveganjerky <99945874+wanghveganjerky@users.noreply.github.com>
2023-09-06 21:02:14 -0400

    1

--- garden.js
+++ garden.js
@@ -72,108 +72,97 @@ export default class Garden {
     }
     
 
-    const keyMap = {
-      A: { value: "A", type: "tagada" },
-      B: { value: "B", type: "tagada" },
-      C: { value: "C", type: "tagada" },
-      D: { value: "D", type: "tagada" },
-      E: { value: "E", type: "tagada" },
-      F: { value: "F", type: "tagada" },
-      G: { value: "G", type: "tagada" },
-      H: { value: "H", type: "tagada" },
-      I: { value: "I", type: "tagada" },
-      J: { value: "J", type: "tagada" },
-      K: { value: "K", type: "tagada" },
-      L: { value: "L", type: "tagada" },
-      M: { value: "M", type: "tagada" },
-      N: { value: "N", type: "tagada" },
-      O: { value: "O", type: "tagada" },
-      P: { value: "P", type: "tagada" },
-      Q: { value: "Q", type: "tagada" },
-      R: { value: "R", type: "tagada" },
-      S: { value: "S", type: "tagada" },
-      T: { value: "T", type: "tagada" },
-      U: { value: "U", type: "tagada" },
-      V: { value: "V", type: "tagada" },
-      W: { value: "W", type: "tagada" },
-      X: { value: "X", type: "tagada" },
-      Y: { value: "Y", type: "tagada" },
-      Z: { value: "Z", type: "tagada" },
-      1: { value: "1", type: "tagada" },
-      2: { value: "2", type: "tagada" },
-      3: { value: "3", type: "tagada" },
-      4: { value: "4", type: "tagada" },
-      5: { value: "5", type: "tagada" },
-      6: { value: "6", type: "tagada" },
-      7: { value: "7", type: "tagada" },
-      8: { value: "8", type: "tagada" },
-      9: { value: "9", type: "tagada" },
-      0: { value: "0", type: "tagada" },
-      a: { value: "a", type: "tagada" },
-      b: { value: "b", type: "tagada" },
-      c: { value: "c", type: "tagada" },
-      d: { value: "d", type: "tagada" },
-      e: { value: "e", type: "tagada" },
-      f: { value: "f", type: "tagada" },
-      g: { value: "g", type: "tagada" },
-      h: { value: "h", type: "tagada" },
-      i: { value: "i", type: "tagada" },
-      j: { value: "j", type: "tagada" },
-      k: { value: "k", type: "tagada" },
-      l: { value: "l", type: "tagada" },
-      m: { value: "m", type: "tagada" },
-      n: { value: "n", type: "tagada" },
-      o: { value: "o", type: "tagada" },
-      p: { value: "p", type: "tagada" },
-      q: { value: "q", type: "tagada" },
-      r: { value: "r", type: "tagada" },
-      s: { value: "s", type: "tagada" },
-      t: { value: "c", type: "tagada" },
-      u: { value: "u", type: "tagada" },
-      v: { value: "v", type: "tagada" },
-      w: { value: "w", type: "tagada" },
-      x: { value: "x", type: "tagada" },
-      y: { value: "y", type: "tagada" },
-      z: { value: "z", type: "tagada" },
+    // const keyMap = {
+    //   A: { value: "A", type: "tagada" },
+    //   B: { value: "B", type: "tagada" },
+    //   C: { value: "C", type: "tagada" },
+    //   D: { value: "D", type: "tagada" },
+    //   E: { value: "E", type: "tagada" },
+    //   F: { value: "F", type: "tagada" },
+    //   G: { value: "G", type: "tagada" },
+    //   H: { value: "H", type: "tagada" },
+    //   I: { value: "I", type: "tagada" },
+    //   J: { value: "J", type: "tagada" },
+    //   K: { value: "K", type: "tagada" },
+    //   L: { value: "L", type: "tagada" },
+    //   M: { value: "M", type: "tagada" },
+    //   N: { value: "N", type: "tagada" },
+    //   O: { value: "O", type: "tagada" },
+    //   P: { value: "P", type: "tagada" },
+    //   Q: { value: "Q", type: "tagada" },
+    //   R: { value: "R", type: "tagada" },
+    //   S: { value: "S", type: "tagada" },
+    //   T: { value: "T", type: "tagada" },
+    //   U: { value: "U", type: "tagada" },
+    //   V: { value: "V", type: "tagada" },
+    //   W: { value: "W", type: "tagada" },
+    //   X: { value: "X", type: "tagada" },
+    //   Y: { value: "Y", type: "tagada" },
+    //   Z: { value: "Z", type: "tagada" },
+    //   1: { value: "1", type: "tagada" },
+    //   2: { value: "2", type: "tagada" },
+    //   3: { value: "3", type: "tagada" },
+    //   4: { value: "4", type: "tagada" },
+    //   5: { value: "5", type: "tagada" },
+    //   6: { value: "6", type: "tagada" },
+    //   7: { value: "7", type: "tagada" },
+    //   8: { value: "8", type: "tagada" },
+    //   9: { value: "9", type: "tagada" },
+    //   0: { value: "0", type: "tagada" },
+    //   a: { value: "a", type: "tagada" },
+    //   b: { value: "b", type: "tagada" },
+    //   c: { value: "c", type: "tagada" },
+    //   d: { value: "d", type: "tagada" },
+    //   e: { value: "e", type: "tagada" },
+    //   f: { value: "f", type: "tagada" },
+    //   g: { value: "g", type: "tagada" },
+    //   h: { value: "h", type: "tagada" },
+    //   i: { value: "i", type: "tagada" },
+    //   j: { value: "j", type: "tagada" },
+    //   k: { value: "k", type: "tagada" },
+    //   l: { value: "l", type: "tagada" },
+    //   m: { value: "m", type: "tagada" },
+    //   n: { value: "n", type: "tagada" },
+    //   o: { value: "o", type: "tagada" },
+    //   p: { value: "p", type: "tagada" },
+    //   q: { value: "q", type: "tagada" },
+    //   r: { value: "r", type: "tagada" },
+    //   s: { value: "s", type: "tagada" },
+    //   t: { value: "c", type: "tagada" },
+    //   u: { value: "u", type: "tagada" },
+    //   v: { value: "v", type: "tagada" },
+    //   w: { value: "w", type: "tagada" },
+    //   x: { value: "x", type: "tagada" },
+    //   y: { value: "y", type: "tagada" },
+    //   z: { value: "z", type: "tagada" },
 
-    };
+    // };
     
-    for (let key in keyMap) {
-      document.addEventListener("keydown", (e) => {
-        if (e.key === key) {
-          this.activeShape.value = keyMap[key].value;
-          this.activeShape.type = keyMap[key].type;
-          this.renderShape();
-        }
-      });
-    }
+    
+    document.addEventListener("keydown", (e) => {
+      if (/^[a-zA-Z0-9]$/.test(e.key)) { // check if the pressed key is alphanumeric
+        this.activeShape.value = e.key; // change the active shape value
+        this.renderShape(); // re-render the shape
+      }
+    });
   }
 
   renderShape() {
     if (this.activeShapeEl === null) {
       this.activeShapeEl = document.createElement("div");
+      this.activeShapeEl.innerHTML = this.activeShape.value;
       this.gardenEl.appendChild(this.activeShapeEl);
     }
-
-    //special case
-    const span = document.createElement('span');
-    span.innerHTML = this.activeShape.value;
+    this.activeShapeEl.innerHTML = this.activeShape.value;
   
     // Special case for descenders
     const descenders = ['g', 'j', 'q', 'y', 'p'];
     if (descenders.includes(this.activeShape.value.toLowerCase())) {
-        this.activeShapeEl.style.borderBottom = '-0.2em solid transparent';  // adjust as needed
-        this.activeShapeEl.style.lineHeight = '0.8em'; // adjust as needed
-    }
-    else {
-        this.activeShapeEl.style.borderBottom = 'none';
-        this.activeShapeEl.style.lineHeight = '1em';
+      this.activeShapeEl.style.paddingBottom = "-10px";  // Adjust this value as needed
+    } else {
+      this.activeShapeEl.style.paddingBottom = "0px";
     }
   
-    // Append the span to the div
-    this.activeShapeEl.innerHTML = '';
-    this.activeShapeEl.appendChild(span);
-
     this.activeShapeEl.style.color = this.activeShape.color; 
     this.activeShapeEl.classList.add("type");
     this.activeShapeEl.style.left =
